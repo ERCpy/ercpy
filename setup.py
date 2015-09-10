12e05a2d68a1a304019128e5d5602fd57629b16c
Martial Duchamp <martial.duchamp@gmail.com>
2015-09-10 15:31:42 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,8 +2,8 @@ from distutils.core import setup
 
 setup(name='test_fib',
 	version='1.0',
-	author = "Martial Duchamp","Jan Caron","Vadim Migunov",
-	author_email = "martial.duchampl@gmail.com","j.caron@fz-juelich.de","v.migunov@fz-juelich.de",
+	author = "Martial Duchamp" "Jan Caron" "Vadim Migunov",
+	author_email = "martial.duchampl@gmail.com" "j.caron@fz-juelich.de" "v.migunov@fz-juelich.de",
 	py_modules=['ercpy.holography'],
 	long_description = """Really long text here.""" 
 	)
