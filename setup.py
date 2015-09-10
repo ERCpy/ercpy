51f26f46569030161c69a73c419cf405797563b8
Martial Duchamp <martial.duchamp@gmail.com>
2015-09-10 16:14:07 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,8 +4,6 @@ setup(name='test_ercpy',
 	version='1.0',
 	author = "Martial Duchamp" "Jan Caron",
 	author_email = "martial.duchampl@gmail.com" "j.caron@fz-juelich.de",
-	py_modules=['ercpy.holography',
-		'ercpy.eelsedx',
-		'ercpy.utils'],
+	py_modules=['ercpy'],
 	long_description = """Really long text here.""" 
 	)
