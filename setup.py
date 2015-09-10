599fc2ae08d4fe15ae156d29fcccef076762ef76
MartialD <martial.duchamp@gmail.com>
2015-09-10 16:01:08 +0200

    First commit

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from distutils.core import setup
 
-setup(name='test_fib',
+setup(name='test_ercpy',
 	version='1.0',
-	author = "Martial Duchamp" "Jan Caron" "Vadim Migunov",
-	author_email = "martial.duchampl@gmail.com" "j.caron@fz-juelich.de" "v.migunov@fz-juelich.de",
+	author = "Martial Duchamp" "Jan Caron",
+	author_email = "martial.duchampl@gmail.com" "j.caron@fz-juelich.de",
 	py_modules=['ercpy.holography',
-		'ercpy.eels-edx',
+		'ercpy.eelsedx',
 		'ercpy.utils'],
 	long_description = """Really long text here.""" 
 	)
