9d3f07119554ebfdbe6f869df0dfdeb1ba8188af
MartialD <martial.duchamp@gmail.com>
2015-09-10 16:32:26 +0200

    First commit

--- setup.py
+++ setup.py
@@ -4,6 +4,8 @@ setup(name='test_ercpy',
 	version='1.0',
 	author = "Martial Duchamp" "Jan Caron",
 	author_email = "martial.duchampl@gmail.com" "j.caron@fz-juelich.de",
-	py_modules=['ercpy'],
+	py_modules=['ercpy.holography',
+		'ercpy.eelsedx',
+		'ercpy.utils'],
 	long_description = """Really long text here.""" 
 	)
