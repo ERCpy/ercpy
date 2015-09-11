f9f6b1b45458f00ce8be07b79d6be60b89b4083f
Jan Caron <j.caron@fz-juelich.de>
2015-09-11 20:59:25 +0200

    First test commit!

---
+++ ercpy/emd.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Sep 11 20:58:25 2015
+
+@author: Jan
+"""
+
+# This is a test file for commit and push and will be used to house the emd reader / writer / class
