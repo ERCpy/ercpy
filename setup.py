c1a6c8a8301346bef5d47540d04b673004d44331
Jan Caron <j.caron@fz-juelich.de>
2015-09-22 14:12:56 +0200

    Added EMC reader/writer! changed __init__.py to automatically load modules! Added config.py (used in emd.py) Added version.py (used in __init__.py)

--- setup.py
+++ setup.py
@@ -1,11 +1,12 @@
+#!python
 from distutils.core import setup
 
 setup(name='test_ercpy',
-	version='1.0',
-	author = "Martial Duchamp" "Jan Caron",
-	author_email = "martial.duchampl@gmail.com" "j.caron@fz-juelich.de",
-	py_modules=['ercpy.holography',
-		'ercpy.eelsedx',
-		'ercpy.utils'],
-	long_description = """Really long text here.""" 
-	)
+      version='1.0',
+      author="Martial Duchamp" "Jan Caron",
+      author_email="martial.duchampl@gmail.com" "j.caron@fz-juelich.de",
+      py_modules=['ercpy.holography',
+                  'ercpy.eelsedx',
+                  'ercpy.utils'],
+      long_description="""Really long text here."""
+      )
